47921a207df37bb1eec06d009d99477f0a386e8a
Danila Bochkarev <bockarevdanila174@gmail.com>
2026-01-30 18:49:41 +0300

    3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -5,11 +5,12 @@
 */
 
 // your code
-let sum = 1
-let i = 20
-while(sum < i){
-  if(sum % 2 !==0){
+let sum = 0
+let i = 1
+while(i <=20){
+  if(i % 2 !==0){
     sum += i;
   }
+  i++
 }
 console.log(sum)
