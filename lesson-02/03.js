6fb1214f6af8fe8aad889dd371d7b55862979305
Danila Bochkarev <bockarevdanila174@gmail.com>
2026-02-04 17:04:28 +0300

    Lesson-2 Task-1

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -5,5 +5,25 @@
  */
 
 const score = 70 // тестовое значение, можно изменять
-let grade
+let grade =  ;
+
+if(score <= 0){
+
+} else if(score >= 0 && score <= 49){
+    grade = 'F'
+
+}else if(score <= 69){
+    grade = 'D'
+
+}else if(score <= 79){
+    grade = 'C'
+
+}else if(score <= 89){
+    grade = 'B'
+
+}else {
+    grade = 'A'
+
+}
+console.log(grade)
 // your code
