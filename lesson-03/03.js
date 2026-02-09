28c8444951f193f87daee6171da4b81b2c7b09a0
Danila Bochkarev <bockarevdanila174@gmail.com>
2026-02-09 14:42:37 +0300

    lesson-3 task-3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,13 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(number1,number2,number3) {
+    if(number1 >= number2 && number1 >= number3){
+        return number1
+    }if(number2 >= number1 && number2 >= number3){
+        return number2
+    }else{
+        return number3
+    }
+}
+    console.log(findLargest(1,15,5))
+
