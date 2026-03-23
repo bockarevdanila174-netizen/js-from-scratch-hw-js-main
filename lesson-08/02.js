f06ef910759845e9089dc9862d956d5e5ff2e36c
Danila Bochkarev <bockarevdanila174@gmail.com>
2026-03-23 16:34:53 +0300

    lesson-8 task-2

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,11 +4,16 @@
  */
 
 function isNumeric(str) {
+  
+    return str.trim() !== "" && Number.isFinite(Number(str))
+  }
+ 
   // your code
-}
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+
+
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false
